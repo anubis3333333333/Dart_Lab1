e1166e0f339799e22fbd9c9d340cedacad3cb4bb
anubis3333333333 <jdkdoekd471@gmail.com>
2026-09-07 13:10:30 +0300

    learn syntax

--- lab1_dart/bin/lab1_dart.dart
+++ lab1_dart/bin/lab1_dart.dart
@@ -1,5 +1,31 @@
-import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
+void main() {
+  String name = "анубис";
+  int age = 20;
+  double h = 1.75;
+  bool isStudent = true;
 
-void main(List<String> arguments) {
-  print('Hello world: ${lab1_dart.calculate()}!');
+  print(name);
+
+  var count = 0; //нельзя пересваивуать значения
+  var title = "Dart";
+  var score = 95;
+  val lan = "alsjdlkasj";
+  print("$lan:$score")//удобнее
+
+  final String language = "dart";
+  final int releaseyear = 2011;
+  language = "py";//низя изменять
+
+  const double pi = 3.14159;//разница в компиляции
+  // String? city = 0;
+  // if (city != null){
+  //   print(city.toUpperCase())
+  // }
+  List<String> fruits = ["банан","апельсин"];
+  fruits.add("груша");
+  print(fruits[0]);
+  print(fruits.length);
+  for (var i in fruits){
+    print(i);
+  }
 }
